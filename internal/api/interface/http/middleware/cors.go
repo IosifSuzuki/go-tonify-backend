c248bc48e8982b88742a43f76b493815111be3c9
Bogdan Petkanich <bogdan.petkanich@nerdpeople.com>
2024-12-17 15:07:38 +0100

    - hotfix: CORS

--- internal/api/interface/http/middleware/cors.go
+++ internal/api/interface/http/middleware/cors.go
@@ -22,7 +22,7 @@ func (c *CORS) CORS() gin.HandlerFunc {
 		ctx.Writer.Header().Set("Access-Control-Allow-Origin", origin)
 		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH")
 		if ctx.Request.Method == http.MethodOptions {
 			ctx.AbortWithStatus(http.StatusNoContent)
 			return
